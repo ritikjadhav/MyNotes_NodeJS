35222363f170835b2e02ec0c655f0da213a9b3bd
Ritik Jadhav <ritiksjadhav99@gmail.com>
2024-02-11 18:58:16 +0530

    Added https server & cookies implementation

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const fs = require("fs");
 const bodyparser = require("body-parser");
 const path = require("path");
+const https = require("https");
+const cookieParser = require("cookie-parser");
+const helmet = require("helmet");
 const route = require("./Routes/route");
 const errorLogger = require("./Utilities/error-logger");
 const requestLogger = require("./Utilities/request-logger");
@@ -10,12 +14,25 @@ const app = express();
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(cookieParser());
+app.use(helmet()); //helmet middleware is a set of 14 small middleware functions that help in setting up security-related HTTP headers with default values
 
 app.use(requestLogger);
 app.use("/", route);
 app.use(errorLogger);
 
-const port = process.env.PORT || 1234;
-app.listen(port, () => {
+// Read the contents of the key and cert files synchronously
+const key = fs.readFileSync("ssl/sshServer.key");
+const cert = fs.readFileSync("ssl/sshServer.cert");
+
+https
+  .createServer(
+    {
+      key: key,
+      cert: cert,
+    },
+    app
+  )
+  .listen(3000, () => {
     console.log("MyNotes Welcomes You!");
-});
+  });
